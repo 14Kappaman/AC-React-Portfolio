a2ff8013ea3b7a9d2db58438a6004a1d1ed9150b
Aubrey Crook <aubreycrook@hotmail.com>
2022-04-18 20:24:53 -0400

    Hotfix fix routes

--- src/navigation.js
+++ src/navigation.js
@@ -1,14 +1,15 @@
+import { Link } from "react-router-dom";
+
 export function Navigation(props) {
     return (
         <header className='App-header'>
         <h1>Welcome to My Portfolio </h1>
 
         <nav>
-
-            <a href="/AC-React-Portfolio">Home</a>
-            <a href="/AC-React-Portfolio/work">Portfolio</a>
-            <a href="/AC-React-Portfolio/resume">Resume</a>
-            <a href="/AC-React-Portfolio/contact">Contact</a>
+            <Link to='/'>Home</Link>
+            <Link to='/work'>Portfolio</Link>
+            <Link to='/resume'>Resume</Link>
+            <Link to='/contact'>Contact</Link>
 
         </nav>
     </header>
@@ -17,3 +18,12 @@ export function Navigation(props) {
 
 //<a href="/about-me">About Me</a>
 //<a href="./assests/AubreyCrookResume2021.pdf" target="_blank">Resume</a>
+
+/*
+
+            <a href="/AC-React-Portfolio">Home</a>
+            <a href="/AC-React-Portfolio/work">Portfolio</a>
+            <a href="/AC-React-Portfolio/resume">Resume</a>
+            <a href="/AC-React-Portfolio/contact">Contact</a>
+
+*/
